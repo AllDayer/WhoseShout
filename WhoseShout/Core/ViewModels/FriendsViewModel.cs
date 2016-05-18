f283f83368654a0666753a39176e28990326772c
alldayer <tristan.drummond@gmail.com>
2016-05-18 19:33:31 +1000

    New files

---
+++ WhoseShout/Core/ViewModels/FriendsViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using MvvmCross.Core.ViewModels;
+using WhoseShout.Helpers;
+using WhoseShout.Services;
+using WhoseShout.Models;
+
+namespace WhoseShout.Core.ViewModels
+{
+	public class FriendsViewModel : MvxViewModel
+	{
+		ObservableCollection<Friend> m_Friends = new ObservableCollection<Friend>();
+		public ObservableCollection<Friend> Friends
+		{
+			get
+			{
+				return m_Friends;
+			}
+			set
+			{
+				m_Friends = value;
+				RaisePropertyChanged(nameof(Friends));
+			}
+		}
+
+		public FriendsViewModel()
+		{
+			ServiceLocator.Instance.Resolve<IService>();
+		}
+
+	}
+}
